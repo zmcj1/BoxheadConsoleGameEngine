19fce8e34d7f8126d817f615fe497f5c08b9d56d
GreatDestroyerCharlie <1669247240@qq.com>
2021-04-04 03:30:02 +0300

    add MinDefines.h

---
+++ src/MinConsoleNative/MinDefines.h
@@ -0,0 +1,77 @@
+﻿#pragma once
+
+//See:https://docs.microsoft.com/en-us/cpp/preprocessor/predefined-macros?view=msvc-160
+
+//Macro:CUI/GUI
+
+#ifdef _MSC_VER
+
+#if !defined(WIN32) && !defined(_WIN32)
+#error Windows Only.
+#endif
+
+#if defined(_MT) && !defined(_DLL) && !defined(_DEBUG)
+#define MT
+#elif defined(_MT) && !defined(_DLL) && defined(_DEBUG)
+#define MT_DEBUG
+#elif defined(_MT) && defined(_DLL) && !defined(_DEBUG)
+#define MD
+#elif defined(_MT) && defined(_DLL) && defined(_DEBUG)
+#define MD_DEBUG
+#endif
+
+//X86/X64
+#ifdef _M_X64
+#define X64
+#else
+#define X86
+#endif
+
+#ifdef __cplusplus
+#define EXPORT_FUNC extern "C" __declspec(dllexport) bool __stdcall
+#else
+#define EXPORT_FUNC extern __declspec(dllexport) bool __stdcall
+#endif
+
+#if defined(CUI)    //(Command User Interface)
+#pragma comment(linker, "/SUBSYSTEM:CONSOLE")
+#elif defined(GUI)  //(Graphical User Interface)
+#pragma comment(linker, "/SUBSYSTEM:WINDOWS")
+#endif
+
+#endif
+
+#if !defined(UNICODE) && !defined(_UNICODE)
+#ifdef _MSC_VER
+#error Please enable UNICODE for your MS compiler! For VisualStudio: Project Properties -> General -> Character Set -> Use Unicode.
+#else
+// For now, I'll try enabling it for you
+#define UNICODE
+#define _UNICODE
+#endif
+#endif
+
+//Disable warning
+#define _CRT_SECURE_NO_WARNINGS
+//Disable network library warning
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <Windows.h>
+
+#ifndef LEN
+#define LEN(arr) (sizeof(arr) / sizeof(arr[0]))
+#endif
+
+#ifndef _T
+#define _T(x) TEXT(x)
+#endif
+
+typedef wchar_t wchar;
+
+typedef unsigned char byte;
+
+typedef unsigned short ushort;
+
+typedef unsigned int uint;
+
+typedef unsigned long long ulong;
