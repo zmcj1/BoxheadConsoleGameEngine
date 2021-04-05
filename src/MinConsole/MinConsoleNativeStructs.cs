4558df210bdb9254a3ae25a233683d4732de551c
GreatDestroyerCharlie <1669247240@qq.com>
2021-04-05 12:15:08 +0300

    ready to make a tool(NativeFunc -> C# funcs automaticly)

---
+++ src/MinConsole/MinConsoleNativeStructs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace MinConsole
+{
+    internal class MinConsoleNativeStructs
+    {
+
+    }
+}
