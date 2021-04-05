a0a3f63ba5c0f6c7c752049264785f79f550458a
GreatDestroyerCharlie <1669247240@qq.com>
2021-04-05 10:21:46 +0300

    Update Console.h

--- src/MinConsoleNative/Console.h
+++ src/MinConsoleNative/Console.h
@@ -1,8 +1,104 @@
 ﻿#pragma once
 
 #include "MinDefines.h"
+#include <utility>
 
 namespace MinConsoleNative
 {
+    enum class ConsoleColor
+    {
+        BLACK = 0,
+        DARKBLUE = 1,
+        DARKGREEN = 2,
+        DARKCYAN = 3,
+        DARKRED = 4,
+        DARKMAGENTA = 5,
+        DARKYELLOW = 6,
+        GRAY = 7,
+        DARKGRAY = 8,
+        BLUE = 9,
+        GREEN = 10,
+        CYAN = 11,
+        RED = 12,
+        MAGENTA = 13,
+        YELLOW = 14,
+        WHITE = 15,
+    };
 
+    inline ushort ConsoleColorToUshort(ConsoleColor foreColor, ConsoleColor backColor)
+    {
+        return (ushort)((ushort)foreColor | ((ushort)backColor << 4));
+    }
+
+    inline std::pair<ConsoleColor, ConsoleColor> UshortToConsoleColor(ushort u)
+    {
+        std::pair<ConsoleColor, ConsoleColor> result;
+        result.first = (ConsoleColor)(u & 0x000F);
+        result.second = (ConsoleColor)((u & 0x00F0) / 16);
+        return result;
+    }
+
+    struct ConsoleInputMode
+    {
+    public:
+        bool _ENABLE_PROCESSED_INPUT;               //default is TRUE(recommend to FALSE to avoid pressing CTRL+C to close the console program)
+        bool _ENABLE_LINE_INPUT;                    //default is TRUE
+        bool _ENABLE_ECHO_INPUT;                    //default is TRUE
+        bool _ENABLE_WINDOW_INPUT;                  //default is FALSE(recommend to TRUE)
+        bool _ENABLE_MOUSE_INPUT;                   //default is TRUE
+        bool _ENABLE_INSERT_MODE;                   //default is TRUE
+        bool _ENABLE_QUICK_EDIT_MODE;               //default is TRUE(recommend to FALSE)
+        bool _ENABLE_EXTENDED_FLAGS;                //default is TRUE
+        bool _ENABLE_AUTO_POSITION;                 //default is TRUE
+        bool _ENABLE_VIRTUAL_TERMINAL_INPUT;        //default is FALSE
+
+        ConsoleInputMode()
+        {
+            this->_ENABLE_PROCESSED_INPUT = true;
+            this->_ENABLE_LINE_INPUT = true;
+            this->_ENABLE_ECHO_INPUT = true;
+            this->_ENABLE_WINDOW_INPUT = false;
+            this->_ENABLE_MOUSE_INPUT = true;
+            this->_ENABLE_INSERT_MODE = true;
+            this->_ENABLE_QUICK_EDIT_MODE = true;
+            this->_ENABLE_EXTENDED_FLAGS = true;
+            this->_ENABLE_AUTO_POSITION = true;
+            this->_ENABLE_VIRTUAL_TERMINAL_INPUT = false;
+        }
+
+        static ConsoleInputMode Standard()
+        {
+            ConsoleInputMode cim;
+            cim._ENABLE_PROCESSED_INPUT = false;
+            cim._ENABLE_WINDOW_INPUT = true;
+            cim._ENABLE_QUICK_EDIT_MODE = false;
+            return cim;
+        }
+    };
+
+    struct ConsoleOutputMode
+    {
+    public:
+        bool _ENABLE_PROCESSED_OUTPUT;              //default is TRUE
+        bool _ENABLE_WRAP_AT_EOL_OUTPUT;            //default is TRUE
+        bool _ENABLE_VIRTUAL_TERMINAL_PROCESSING;   //default is FALSE(recommend to TRUE, If want to use the VT100 sequence)
+        bool _DISABLE_NEWLINE_AUTO_RETURN;          //default is FALSE
+        bool _ENABLE_LVB_GRID_WORLDWIDE;            //default is FALSE
+
+        ConsoleOutputMode()
+        {
+            this->_ENABLE_PROCESSED_OUTPUT = true;
+            this->_ENABLE_WRAP_AT_EOL_OUTPUT = true;
+            this->_ENABLE_VIRTUAL_TERMINAL_PROCESSING = false;
+            this->_DISABLE_NEWLINE_AUTO_RETURN = false;
+            this->_ENABLE_LVB_GRID_WORLDWIDE = false;
+        }
+
+        static ConsoleOutputMode Standard()
+        {
+            ConsoleOutputMode com;
+            com._ENABLE_VIRTUAL_TERMINAL_PROCESSING = true;
+            return com;
+        }
+    };
 }
