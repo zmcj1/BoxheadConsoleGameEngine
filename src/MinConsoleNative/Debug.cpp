c8d7381ff23a4877fb539f35c8fd85b6ea8b3acf
GreatDestroyerCharlie <1669247240@qq.com>
2021-04-06 21:21:49 +0300

    update Log function

--- src/MinConsoleNative/Debug.cpp
+++ src/MinConsoleNative/Debug.cpp
@@ -1,14 +1,49 @@
 ﻿#include "Debug.h"
+#include "File.h"
+#include "String.h"
+#include <ctime>
+using namespace std;
 
 namespace MinConsoleNative
 {
     void Debug::Output(const std::wstring& msg)
     {
-        OutputDebugString(msg.c_str());
+        ::OutputDebugString(msg.c_str());
     }
 
     void Debug::OutputLine(const std::wstring& msg)
     {
-        OutputDebugString((msg + L"\n").c_str());
+        ::OutputDebugString((msg + L"\n").c_str());
+    }
+
+    void Debug::Log(const std::wstring& path, const std::wstring& msg, MessageType msgType)
+    {
+        FileMode logFileMode = File::Status(path);
+        if (logFileMode != FileMode::File)
+        {
+            OutputLine(L"Create file:" + path);
+            File::Creat(path, FileMode::File);
+        }
+
+        wstring line;
+
+        time_t now = time(0);
+        char* str = ctime(&now); //Shit! This str with '\n'.
+        wstring time_wstr = String::StringToWstring(str);
+
+        switch (msgType)
+        {
+        case MessageType::Message:
+            line = _T("\"") + msg + _T("\" -message ") + _T("----") + time_wstr;
+            break;
+        case MessageType::Warning:
+            line = _T("\"") + msg + _T("\" -warning ") + _T("----") + time_wstr;
+            break;
+        case MessageType::Error:
+            line = _T("\"") + msg + _T("\" -error ") + _T("----") + time_wstr;
+            break;
+        }
+
+        File::WriteAllText(path, line, WriteMode::Append);
     }
 }
--- src/MinConsoleNative/Debug.h
+++ src/MinConsoleNative/Debug.h
@@ -5,11 +5,23 @@
 
 namespace MinConsoleNative
 {
+    enum class MessageType
+    {
+        Message = 1,
+        Warning = 2,
+        Error = 3,
+    };
+
     class Debug
     {
     public:
+        //Output to IDE
         static void Output(const std::wstring& msg);
 
+        //Output to IDE
         static void OutputLine(const std::wstring& msg);
+
+        //Log to file, if the file is not exsits I will create it.
+        static void Log(const std::wstring& path, const std::wstring& msg, MessageType msgType = MessageType::Message);
     };
 }
