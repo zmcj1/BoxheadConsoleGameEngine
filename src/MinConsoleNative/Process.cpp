cafdb937a32c299da9fcd9933a03626b46259f54
GreatDestroyerCharlie <1669247240@qq.com>
2021-04-08 08:53:25 +0300

    add class Process

---
+++ src/MinConsoleNative/Process.cpp
@@ -0,0 +1,10 @@
+﻿#include "Process.h"
+#include <shellapi.h>
+
+namespace MinConsoleNative
+{
+    HINSTANCE Process::Execute(const std::wstring& file, const std::wstring& param)
+    {
+        return ::ShellExecute(nullptr, _T("open"), file.c_str(), param.c_str(), nullptr, SW_SHOW);
+    }
+}
---
+++ src/MinConsoleNative/Process.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "MinDefines.h"
+#include <string>
+
+namespace MinConsoleNative
+{
+    class Process
+    {
+    public:
+        //Parameters : chrome.exe https://www.youtube.com/
+        static HINSTANCE Execute(const std::wstring& file, const std::wstring& param);
+    };
+}
