d390fb73bae8b8bcaf1f7c7f582baec2b4461f14
GreatDestroyerCharlie <1669247240@qq.com>
2021-04-09 18:38:20 +0300

    update TextLayout class

--- src/MinConsoleNative/TextLayout.cpp
+++ src/MinConsoleNative/TextLayout.cpp
@@ -1,6 +1,63 @@
 ﻿#include "TextLayout.h"
+#include "String.h"
+
+using namespace std;
 
 namespace MinConsoleNative
 {
+    TextLayout::TextLayout(Console console)
+    {
+        this->console = console;
+    }
 
+    wstring TextLayout::CombineWchar(wchar a1, wchar a2)
+    {
+        wchar arr[3] = { 0 };
+        arr[0] = a1;
+        arr[1] = a2;
+        return wstring(arr);
+    }
+
+    std::vector<std::wstring> TextLayout::WstringToGrids(const std::wstring wstr)
+    {
+        vector<wstring> grids;
+        bool hasTemp = false;
+        wchar temp;
+
+        for (int i = 0; i < wstr.length(); i++)
+        {
+            wchar item = wstr[i];
+            CharWidth charWidth = console.GetWcharWidth(item);
+            if (charWidth == CharWidth::Half)
+            {
+                if (hasTemp)
+                {
+                    grids.push_back(CombineWchar(temp, item));
+                    hasTemp = false;
+                }
+                else
+                {
+                    temp = item;
+                    hasTemp = true;
+                }
+            }
+            if (charWidth == CharWidth::Full)
+            {
+                if (hasTemp)
+                {
+                    grids.push_back(CombineWchar(temp, _T(' ')));
+                    hasTemp = false;
+                }
+                grids.push_back(String::WcharToWstring(item));
+            }
+        }
+
+        if (hasTemp)
+        {
+            grids.push_back(CombineWchar(temp, _T(' ')));
+            hasTemp = false;
+        }
+
+        return grids;
+    }
 }
--- src/MinConsoleNative/TextLayout.h
+++ src/MinConsoleNative/TextLayout.h
@@ -1,8 +1,23 @@
 ﻿#pragma once
 
 #include "MinDefines.h"
+#include "Console.h"
+#include <string>
+#include <vector>
+#include <map>
 
 namespace MinConsoleNative
 {
+    class TextLayout
+    {
+    public:
+        static std::wstring CombineWchar(wchar a1, wchar a2);
 
+    public:
+        Console console;
+
+        TextLayout(Console console);
+
+        std::vector<std::wstring> WstringToGrids(const std::wstring wstr);
+    };
 }
