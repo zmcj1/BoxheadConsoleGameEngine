b56d397888a7d097b5587fdc2552bd26aaa2b8dc
GreatDestroyerCharlie <1669247240@qq.com>
2021-04-09 21:10:30 +0300

    update Vector2

--- src/MinConsoleNative/Vector2.cpp
+++ src/MinConsoleNative/Vector2.cpp
@@ -1,6 +1,97 @@
 ﻿#include "Vector2.h"
 
+using namespace std;
+
 namespace MinConsoleNative
 {
+    const Vector2 Vector2::zero = { 0, 0 };
 
+    int Vector2::Distance(const Vector2& a, const Vector2& b)
+    {
+        return ::abs(a.x - b.x) + ::abs(a.y - b.y);
+    }
+
+    Vector2::Vector2() :x(0), y(0)
+    {
+    }
+
+    Vector2::Vector2(int x, int y) : x(x), y(y)
+    {
+    }
+
+    Vector2 Vector2::operator-()
+    {
+        return Vector2(-x, -y);
+    }
+
+    bool Vector2::operator==(const Vector2& other)
+    {
+        if (x == other.x && y == other.y)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool Vector2::operator!=(const Vector2& other)
+    {
+        if (x != other.x || y != other.y)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    Vector2 Vector2::operator+(const Vector2& other)
+    {
+        Vector2 vector;
+        vector.x = x + other.x;
+        vector.y = y + other.y;
+        return vector;
+    }
+
+    Vector2 Vector2::operator-(const Vector2& other)
+    {
+        Vector2 vector;
+        vector.x = x - other.x;
+        vector.y = y - other.y;
+        return vector;
+    }
+
+    Vector2 Vector2::operator*(int s)
+    {
+        Vector2 vector;
+        vector.x = x * s;
+        vector.y = y * s;
+        return vector;
+    }
+
+    Vector2 Vector2::operator/(int s)
+    {
+        Vector2 vector;
+        if (s == 0)
+        {
+            throw "Divide by zero exception";
+        }
+        vector.x = x / s;
+        vector.y = y / s;
+        return vector;
+    }
+
+    wstring Vector2::ToString()
+    {
+        wstring wstr;
+        wstr += _T("[X:");
+        wstr += to_wstring(x);
+        wstr += _T(" Y:");
+        wstr += to_wstring(y);
+        wstr += _T("]");
+        return wstr;
+    }
 }
--- src/MinConsoleNative/Vector2.h
+++ src/MinConsoleNative/Vector2.h
@@ -1,8 +1,40 @@
 ﻿#pragma once
 
 #include "MinDefines.h"
+#include <string>
 
 namespace MinConsoleNative
 {
+    class Vector2
+    {
+    public:
+        static const Vector2 zero;
 
+        static int Distance(const Vector2& a, const Vector2& b);
+
+    public:
+        int x;
+        int y;
+
+    public:
+        Vector2();
+
+        Vector2(int x, int y);
+
+        Vector2 operator-();
+
+        bool operator ==(const Vector2& other);
+
+        bool operator !=(const Vector2& other);
+
+        Vector2 operator+(const Vector2& other);
+
+        Vector2 operator-(const Vector2& other);
+
+        Vector2 operator*(int s);
+
+        Vector2 operator/(int s);
+
+        std::wstring ToString();
+    };
 }
