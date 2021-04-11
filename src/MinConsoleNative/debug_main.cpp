73d2b449f7a64917f49ee5112e90566fe5881af5
GreatDestroyerCharlie <1669247240@qq.com>
2021-04-11 21:49:35 +0300

    try to learn Direct2D and DirectWrite

--- src/MinConsoleNative/debug_main.cpp
+++ src/MinConsoleNative/debug_main.cpp
@@ -9,13 +9,73 @@ int main()
 {
     POINT pos = Window::Global.GetInstance().GetCenterPosOfWindowInDesktop();
     Window::Global.GetInstance().SetWindowPos(pos);
-
     Console::Global.GetInstance().SetTreatControlCAsInput(true);
 
-    Console::Global.GetInstance().WriteLine(L"Hello World!");
 
-    Console::Global.GetInstance().ReadLine();
-    return 0;
+    //while (true)
+    //{
+    //    Input::CheckKeyboardEx();
+    //    if (Input::GetKeyDownEx('A'))
+    //    {
+    //        Console::Global.GetInstance().WriteLine(L"down");
+    //    }
+    //    if (Input::GetKeyUpEx('A'))
+    //    {
+    //        Console::Global.GetInstance().WriteLine(L"up");
+    //    }
+    //    Sleep(1);
+    //}
+
+
+    //CHAR_INFO cells[6];
+    //for (int i = 0; i < 6; i++)
+    //{
+    //    if (i % 2 == 0)
+    //    {
+    //        cells[i].Char.UnicodeChar = L'大';
+    //        cells[i].Attributes = ConsoleColorToUshort(ConsoleColor::RED, ConsoleColor::GREEN) |
+    //            COMMON_LVB_LEADING_BYTE;
+    //    }
+    //    else
+    //    {
+    //        cells[i].Char.UnicodeChar = L'\0';
+    //        cells[i].Attributes = ConsoleColorToUshort(ConsoleColor::RED, ConsoleColor::GREEN) |
+    //            COMMON_LVB_TRAILING_BYTE;
+    //    }
+    //    cells[i].Attributes |= COMMON_LVB_UNDERSCORE;
+    //}
+    //auto xxx = Console::Global.GetInstance().GetConsoleFont();
+    //COORD SIZE = GetConsoleFontSize(Console::Global.GetInstance().cons.consoleOutput, xxx.FontIndex);
+    //Console::Global.GetInstance().WriteConsoleOutputW(cells, 0, 0, 6, 1);
+    //Console::Global.GetInstance().SetConsoleCursorPos({ 10,0 });
+    //Console::Global.GetInstance().ReadLine();
+    //return 0;
+
+
+    //ConsoleFont cf;
+    //cf.SetFontBold(false);
+    //cf.SetFontWidth(8);
+    //cf.SetFontHeight(16);
+    //cf.SetFontName(L"Consolas");
+    //Console::Global.GetInstance().SetConsoleFont(cf);
+    //Console::Global.GetInstance().GetConsoleFont();
+    //TextLayout textLayout(Console::Global.GetInstance());
+    //auto lines = textLayout.WstringToLines(L"english中文русский", 5);
+    //for (size_t i = 0; i < lines.size(); i++)
+    //{
+    //    wstring line = lines[i];
+    //    Console::Global.GetInstance().WriteConsoleOutputCharacterW(line, { 0 , (short)i });
+    //    ushort att[5];
+    //    for (size_t i = 0; i < LEN(att); i++)
+    //    {
+    //        att[i] = ConsoleColorToUshort(ConsoleColor::RED, ConsoleColor::BLUE);
+    //    }
+    //    Console::Global.GetInstance().WriteConsoleOutputAttribute(att, 5, { 0 , (short)i });
+    //}
+    //Console::Global.GetInstance().ReadLine();
+    //return 0;
+
+
     //MinTimer timer;
     //MinInitTimer(&timer);
     //while (true)
@@ -27,6 +87,7 @@ int main()
     //    MinTimeTimer(&timer, &deltaTime);
     //    cout << deltaTime << "\n";
     //}
+
 
     //Input::ResetMouseAxis();
     //Timer timer;
---
+++ src/MinConsoleNative/win_main.cpp
@@ -0,0 +1,196 @@
+﻿#ifdef _DEBUG
+
+#define GUI
+#include "MinDefines.h"
+
+//FOR WIN32 SEE:https://docs.microsoft.com/en-us/windows/win32/learnwin32/your-first-windows-program
+
+#include <d2d1.h>
+#include <dwrite.h>
+#pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "dwrite.lib")
+
+#include <string>
+using namespace std;
+
+//D2D & DW factories
+
+ID2D1Factory* pD2DFactory_;
+ID2D1HwndRenderTarget* pRT_;
+ID2D1SolidColorBrush* pBlackBrush_;
+
+IDWriteFactory* pDWriteFactory_;
+IDWriteTextFormat* pTextFormat_;
+
+wstring wstr = L"asdkjasld啊是大多数空间";
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+    // Register the window class.
+    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window.
+
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        L"Learn to Program Windows",    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd); //test
+
+    //Init my DirectWrite and Direct2d factories.
+
+    HRESULT hr;
+
+    hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &pD2DFactory_);
+
+    if (SUCCEEDED(hr))
+    {
+        hr = DWriteCreateFactory(
+            DWRITE_FACTORY_TYPE_SHARED,
+            __uuidof(IDWriteFactory),
+            reinterpret_cast<IUnknown**>(&pDWriteFactory_)
+        );
+    }
+
+    if (SUCCEEDED(hr))
+    {
+        //hr = pDWriteFactory_->CreateTextFormat(
+        //	L"Oswald",
+        //	NULL,
+        //	DWRITE_FONT_WEIGHT_NORMAL,
+        //	DWRITE_FONT_STYLE_NORMAL,
+        //	DWRITE_FONT_STRETCH_NORMAL,
+        //	20.0f,
+        //	L"en-us", &pTextFormat_);
+        hr = pDWriteFactory_->CreateTextFormat(
+            L"Consolas",
+            NULL,
+            DWRITE_FONT_WEIGHT_NORMAL,
+            DWRITE_FONT_STYLE_NORMAL,
+            DWRITE_FONT_STRETCH_NORMAL,
+            50.0f,
+            L"en-us", &pTextFormat_);
+    }
+
+    if (SUCCEEDED(hr))
+    {
+        hr = pTextFormat_->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_LEADING);
+    }
+
+    if (SUCCEEDED(hr))
+    {
+        hr = pTextFormat_->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_CENTER);
+    }
+
+
+
+    RECT rc;
+    GetClientRect(hwnd, &rc);
+
+    D2D1_SIZE_U size = D2D1::SizeU(rc.right - rc.left, rc.bottom - rc.top);
+
+    if (!pRT_)
+    {
+        D2D1_HWND_RENDER_TARGET_PROPERTIES hRTP = D2D1::HwndRenderTargetProperties(hwnd, size);
+
+        hr = pD2DFactory_->CreateHwndRenderTarget(
+            D2D1::RenderTargetProperties(), hRTP, &pRT_);
+    }
+
+    if (SUCCEEDED(hr))
+    {
+        hr = pRT_->CreateSolidColorBrush(
+            D2D1::ColorF(D2D1::ColorF::White),
+            &pBlackBrush_);
+    }
+
+
+
+    GetClientRect(hwnd, &rc);
+
+    HDC screen = GetDC(hwnd);
+    float dpiScaleX_ = GetDeviceCaps(screen, LOGPIXELSX) / 96.0f;
+    float dpiScaleY_ = GetDeviceCaps(screen, LOGPIXELSY) / 96.0f;
+
+    D2D1_RECT_F layoutRect = D2D1::RectF(rc.left, rc.top, rc.right, rc.bottom);
+
+    //layoutRect = D2D1::RectF(
+    //    static_cast<FLOAT>(rc.left) / dpiScaleX_,
+    //    static_cast<FLOAT>(rc.top) / dpiScaleY_,
+    //    static_cast<FLOAT>(rc.right) / dpiScaleX_,
+    //    static_cast<FLOAT>(rc.bottom) / dpiScaleY_);
+
+    pRT_->BeginDraw();
+
+    //pRT_->Clear(D2D1::ColorF(D2D1::ColorF::White));
+    pRT_->Clear(D2D1::ColorF(D2D1::ColorF::Black));
+    pRT_->DrawText(wstr.c_str(), wstr.size(), pTextFormat_, layoutRect, pBlackBrush_);
+
+    hr = pRT_->EndDraw();
+
+
+    // Run the message loop.
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+
+
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+        EndPaint(hwnd, &ps);
+    }
+    return 0;
+
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+#endif
