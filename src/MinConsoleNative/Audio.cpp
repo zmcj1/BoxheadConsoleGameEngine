15fb4a33e3080f7d25695e70a41502c6dbba45a2
GreatDestroyerCharlie <1669247240@qq.com>
2021-04-18 07:55:10 +0300

    update Audio class

--- src/MinConsoleNative/Audio.cpp
+++ src/MinConsoleNative/Audio.cpp
@@ -1,6 +1,124 @@
 ﻿#include "Audio.h"
+#include "File.h"
+//Provide MCI interface, mmsystem header file depends on Windows.h
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+
+using namespace std;
 
 namespace MinConsoleNative
 {
+    EXPORT_FUNC_EX(bool) MinMCISendString(_IN_ const wchar* str)
+    {
+        MCIERROR mci_result = ::mciSendString(str, nullptr, 0, nullptr);
+        return mci_result == 0;
+    }
 
+    EXPORT_FUNC_EX(bool) MinMCISendString2(_IN_ const wchar* str, _OUT_ wchar* returnStr, int returnStrLen)
+    {
+        MCIERROR mci_result = ::mciSendString(str, returnStr, returnStrLen, nullptr);
+        return mci_result == 0;
+    }
+
+    EXPORT_FUNC_EX(bool) MinPlaySound(_IN_ const wchar* path, bool repeatPlay)
+    {
+        if (repeatPlay)
+        {
+            return ::PlaySound(path, nullptr, SND_FILENAME | SND_ASYNC | SND_LOOP);
+        }
+        else
+        {
+            return ::PlaySound(path, nullptr, SND_FILENAME | SND_ASYNC);
+        }
+    }
+
+    bool Audio::MCISendString(const wstring& cmd)
+    {
+        return MinMCISendString(cmd.c_str());
+    }
+
+    wstring Audio::MCISendString2(const wstring& cmd)
+    {
+        wchar res[MAX_PATH] = { 0 };
+        MinMCISendString2(cmd.c_str(), res, MAX_PATH);
+        wstring result(res);
+        return result;
+    }
+
+    Audio::Audio(const wstring& path, int defaultVolume)
+    {
+        this->path = path;
+        this->short_path_name = File::ToShortPathName(path);
+
+        this->paused = !MCISendString(_T("open ") + this->short_path_name);
+
+        SetVolume(defaultVolume);
+
+        //get the length of the audio
+        wstring result = MCISendString2(_T("status ") + this->short_path_name + _T(" length"));
+
+        int totalMilliSecond = ::_wtoi(result.c_str());
+
+        this->minute = (int)(totalMilliSecond / 1000 / 60);
+        this->second = totalMilliSecond / 1000 - this->minute * 60;
+        this->milliSecond = totalMilliSecond % 1000;
+    }
+
+    Audio::~Audio()
+    {
+        MCISendString(_T("close ") + this->short_path_name);
+    }
+
+    bool Audio::Play(bool repeat)
+    {
+        wstring cmd;
+        if (repeat)
+        {
+            cmd = _T("play ") + this->short_path_name + _T(" repeat");
+        }
+        else
+        {
+            cmd = _T("play ") + this->short_path_name;
+        }
+        this->paused = !MCISendString(cmd);
+        return !this->paused;
+    }
+
+    void Audio::Pause()
+    {
+        this->paused = MCISendString(_T("pause ") + this->short_path_name);
+    }
+
+    int Audio::GetVolume()
+    {
+        return this->volume;
+    }
+
+    void Audio::SetVolume(int volume)
+    {
+        bool suc = MCISendString(_T("setaudio ") + this->short_path_name + _T(" volume to ") + to_wstring(volume));
+
+        if (suc)
+        {
+            this->volume = volume;
+        }
+    }
+
+    int Audio::GetPosition()
+    {
+        wstring r = MCISendString2(_T("status ") + this->short_path_name + _T(" position"));
+
+        int pos = _wtoi(r.c_str());
+        return pos;
+    }
+
+    void Audio::SetPosition(int milliSecond)
+    {
+        MCISendString(_T("seek ") + this->short_path_name + _T(" to ") + to_wstring(milliSecond));
+    }
+
+    wstring Audio::GetMode()
+    {
+        return MCISendString2(_T("status ") + this->short_path_name + _T(" mode"));
+    }
 }
--- src/MinConsoleNative/Audio.h
+++ src/MinConsoleNative/Audio.h
@@ -1,8 +1,64 @@
 ﻿#pragma once
 
 #include "MinDefines.h"
+#include <string>
+
+//SEE:https://docs.microsoft.com/en-us/windows/win32/multimedia/mci
 
 namespace MinConsoleNative
 {
+    EXPORT_FUNC_EX(bool) MinMCISendString(_IN_ const wchar* str);
 
+    EXPORT_FUNC_EX(bool) MinMCISendString2(_IN_ const wchar* str, _OUT_ wchar* returnStr, int returnStrLen);
+
+    //This method is very simple and only supports playing .wav files
+    EXPORT_FUNC_EX(bool) MinPlaySound(_IN_ const wchar* path, bool repeatPlay);
+
+    class Audio
+    {
+    public:
+        static const int MIN_VOLUME = 0;
+
+        static const int MAX_VOLUME = 1000;
+
+        static bool MCISendString(const std::wstring& cmd);
+
+        static std::wstring MCISendString2(const std::wstring& cmd);
+
+    public:
+        Audio(const std::wstring& path, int defaultVolume = MAX_VOLUME);
+
+        ~Audio();
+
+        //play audio(.mp3, .wav etc) this function wont block the main thread.
+        //NOTICE:if play .wav music, you should not repeat.
+        bool Play(bool repeat = false);
+
+        void Pause();
+
+        //set volume [0-1000]
+        int GetVolume();
+
+        //set volume [0-1000]
+        void SetVolume(int volume);
+
+        //get audio current position (in milliSecond)
+        int GetPosition();
+
+        //seek to (in milliSecond) after calling this API, you need to use Play to start playing audio.
+        void SetPosition(int milliSecond);
+
+        //get status return(playing paused stopped)
+        std::wstring GetMode();
+
+        std::wstring path;
+        std::wstring short_path_name;
+        bool paused;
+
+        int minute;         //minute part of this audio
+        int second;         //second part of this audio
+        int milliSecond;    //milliSecond part of this audio
+    private:
+        int volume;
+    };
 }
