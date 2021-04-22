3d518cdd9f832d098a19ac6e23106d99b2bda7d7
GreatDestroyerCharlie <1669247240@qq.com>
2021-04-22 04:03:46 +0300

    Update Maze.h

--- src/MinConsoleNative/Maze.h
+++ src/MinConsoleNative/Maze.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "MinDefines.h"
+#include "Vector.h"
 #include "Vector2.h"
 #include "Random.h"
 #include "Debug.h"
@@ -10,9 +11,55 @@ namespace MinConsoleNative
 {
     class MazeGenerator
     {
+    private:
+        static void LinkTo(Vector2& current, std::vector<Vector2>& availablePos, std::vector<Vector2>& arrivedPos, std::vector<Vector2>& routePos)
+        {
+            std::vector<Vector2> canLinkPos;
+
+            if (Vector<Vector2>::Contains(availablePos, current + Vector2::up * 2))
+            {
+                canLinkPos.push_back(current + Vector2::up * 2);
+            }
+            if (Vector<Vector2>::Contains(availablePos, current + Vector2::down * 2))
+            {
+                canLinkPos.push_back(current + Vector2::down * 2);
+            }
+            if (Vector<Vector2>::Contains(availablePos, current + Vector2::left * 2))
+            {
+                canLinkPos.push_back(current + Vector2::left * 2);
+            }
+            if (Vector<Vector2>::Contains(availablePos, current + Vector2::right * 2))
+            {
+                canLinkPos.push_back(current + Vector2::right * 2);
+            }
+
+            if (!Vector<Vector2>::Contains(arrivedPos, current))
+            {
+                Vector<Vector2>::Remove(availablePos, current);
+                arrivedPos.push_back(current);
+            }
+
+            if (canLinkPos.size() == 0)
+            {
+                Vector<Vector2>::Remove(arrivedPos, current);
+
+                if (arrivedPos.size() == 0) return;
+                else
+                {
+                    int r = Random::Range(0, arrivedPos.size() - 1);
+                    LinkTo(arrivedPos[r], availablePos, arrivedPos, routePos);
+                }
+            }
+            else
+            {
+                int r = Random::Range(0, canLinkPos.size() - 1);
+                Vector2 nextPos = canLinkPos[r];
+                routePos.push_back((current + nextPos) / 2);
+                LinkTo(nextPos, availablePos, arrivedPos, routePos);
+            }
+        }
+
     public:
-
-
         static std::vector<Vector2> GenerateMaze(int width, int height)
         {
             std::vector<Vector2> pathPositions;
@@ -38,14 +85,7 @@ namespace MinConsoleNative
 
             Vector2 startPos = availablePos[Random::Range(0, availablePos.size() - 1)];
 
-            auto func = [](Vector2 current)
-            {
-                std::vector<Vector2> canLinkPos;
-
-
-            };
-
-            func(startPos);
+            LinkTo(startPos, availablePos, arrivedPos, routePos);
 
             for (int i = 0; i < routePos.size(); i++)
             {
