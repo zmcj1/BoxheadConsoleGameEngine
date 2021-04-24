4899184e27a20cf5fb15fa88f808c7aaaac0a75c
GreatDestroyerCharlie <1669247240@qq.com>
2021-04-24 12:13:38 +0300

    update

--- src/MinConsoleNativeExample/Dev.cpp
+++ src/MinConsoleNativeExample/Dev.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
+    ConsoleEngine ce;
+    ce.ConstructConsole(L"dev", PaletteType::Legacy, 60, 30, FontSize::_10x20);
+    console.ReadLine();
+
     int x = GetSystemMetrics(SM_CXSCREEN) / 2;
     int y = GetSystemMetrics(SM_CYSCREEN) / 2;
 
