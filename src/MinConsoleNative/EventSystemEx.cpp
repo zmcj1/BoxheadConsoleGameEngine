f93bad3b03a313e20dc599e49f2682f8272e5ede
GreatDestroyerCharlie <1669247240@qq.com>
2021-04-28 09:11:00 +0300

    ready to make EventSystemEx

---
+++ src/MinConsoleNative/EventSystemEx.cpp
@@ -0,0 +1,6 @@
+﻿#include "EventSystemEx.h"
+
+namespace MinConsoleNative
+{
+
+}
---
+++ src/MinConsoleNativeExample/NewInputModuleExample.cpp
@@ -0,0 +1,11 @@
+﻿#include "..\\MinConsoleNative\MinConsoleNative.h"
+
+using namespace std;
+
+int main()
+{
+
+
+
+    return 0;
+}
