269c2bbbad4877283311c1b7dee39fef31c5ac28
GreatDestroyerCharlie <1669247240@qq.com>
2021-04-30 13:50:15 +0300

    Update Utils.h

--- src/MinConsoleNative/Utils.h
+++ src/MinConsoleNative/Utils.h
@@ -2,9 +2,8 @@
 
 #include "MinDefines.h"
 
-//NOTICE:Most of the methods here are copy from Internet.
 namespace MinConsoleNative
 {
-    //SEE:https://blog.csdn.net/thedarkfairytale/article/details/73457200 with Google translate.
+    //SEE:https://blog.csdn.net/thedarkfairytale/article/details/73457200
     bool is_str_utf8(const char* str);
 }
