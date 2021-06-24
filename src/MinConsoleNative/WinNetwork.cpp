8e0c7544d3877677564804c9f40315222726fbe5
GreatDestroyerCharlie <1669247240@qq.com>
2021-06-24 11:58:57 +0300

    update

--- src/MinConsoleNative/WinNetwork.cpp
+++ src/MinConsoleNative/WinNetwork.cpp
@@ -6,12 +6,12 @@ namespace MinConsoleNative
     EXPORT_FUNC_EX(bool) MinInitWinSocket()
     {
         WSADATA data;
-        int r = WSAStartup(MAKEWORD(2, 2), &data);
+        int r = ::WSAStartup(MAKEWORD(2, 2), &data);
         return r == 0;
     }
 
     EXPORT_FUNC_EX(bool) MinReleaseWinSocket()
     {
-        return WSACleanup() == 0;
+        return ::WSACleanup() == 0;
     }
 }
--- src/MinConsoleNativeExample/sgs.cpp
+++ src/MinConsoleNativeExample/sgs.cpp
@@ -3,28 +3,73 @@
 
 using namespace std;
 
+class Wujiang
+{
+public:
+    wstring name;
+    int level;
+    int weight;
+
+    Wujiang(wstring name, int level, int weight)
+    {
+        this->name = name;
+        this->level = level;
+        this->weight = weight;
+    }
+};
+
 int main()
 {
-    MinInitWinSocket();
+    wstring resPath = L"../../res/";
+    wstring wujiangWeightPath = File::Combine(resPath, L"sgs.txt");
+    wstring bgmPath = File::Combine(resPath, L"sgs_bgm.mp3");
 
+    bool sgs_exists = File::Exists(wujiangWeightPath);
+    bool bgm_exists = File::Exists(bgmPath);
 
-    ConsoleEngine ce;
-    ce.ConstructConsole(L"三国杀抽卡", PaletteType::Legacy, 60, 30, FontSize::_10x20);
-
-    GDIRenderer::PutPixel(100, 100, Color24(255, 255, 255));
-
-    lua_test();
-
-    wstring configPath = L"../../res/sgs.txt";
-    if (!File::Exists(configPath))
+    if (!sgs_exists)
     {
-        console.WriteLine(L"找不到sgs.txt的路径！", ConsoleColor::RED);
-        console.ReadLine();
-        return -1;
+        throw "Can't find sgs.txt!";
+    }
+    if (!bgm_exists)
+    {
+        throw "Can't find sgs_bgm.mp3!";
     }
 
-    console.ReadKey();
+    ConsoleEngine ce;
+    ce.ConstructConsole(L"三国杀", PaletteType::Legacy, 60, 30, FontSize::_10x20);
 
-    MinReleaseWinSocket();
+    Audio bgmPlayer(bgmPath);
+    bgmPlayer.Play(true, false);
+
+    vector<wstring> wujiangLines = File::ReadAllLines(wujiangWeightPath);
+    vector<Wujiang> wujiangs;
+
+    for (int i = 0; i < wujiangLines.size(); i++)
+    {
+        //trim
+        wstring item = String::TrimAll(wujiangLines[i]);
+        //ignore //
+        if (item.find(L"//") != wstring::npos)
+        {
+            continue;
+        }
+        //ignore empty line
+        if (item == L"")
+        {
+            continue;
+        }
+        //split
+        vector<wstring> wujiangLine = String::Split(item, L",");
+        //add
+        wujiangs.push_back(Wujiang(wujiangLine[0],
+            _wtoi(wujiangLine[1].c_str()), _wtoi(wujiangLine[2].c_str())));
+    }
+
+    //legacy
+    //GDIRenderer::PutPixel(100, 100, Color24(255, 255, 255));
+    //lua_test();
+
+    console.ReadKey();
     return 0;
 }
