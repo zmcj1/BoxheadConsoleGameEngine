0d7fb8a1c5fed6dfee721485bbfafe08a8905664
GreatDestroyerCharlie <1669247240@qq.com>
2021-11-12 08:14:00 +0300

    简化Win32，删除过于复杂的函数

--- src/MinConsole/src/Win32.cs
+++ src/MinConsole/src/Win32.cs
@@ -9,11 +9,6 @@
 
         public const string KERNEL32 = "kernel32.dll";
 
-        public const string WINMM = "winmm.dll";
-
-        [DllImport(WINMM, SetLastError = true, CharSet = CharSet.Unicode)]
-        public static extern uint mciSendString(string lpstrCommand, string lpstrReturnString, uint uReturnLength, IntPtr hwndCallback);
-
         [DllImport(USER32, SetLastError = true)]
         public static extern short GetAsyncKeyState(int vKey);
 
