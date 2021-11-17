7761f5de47cedc8ed550fa85713194e599472506
GreatDestroyerCharlie <1669247240@qq.com>
2021-11-17 08:01:03 +0300

    Update AudioPool.hpp

--- src/MinConsoleNative/AudioPool.hpp
+++ src/MinConsoleNative/AudioPool.hpp
@@ -5,6 +5,11 @@
 #include <string>
 #include <vector>
 
+//原计划使用多线程技术提升AudioPool的效率
+//但是因为MCI的限制(如果想在多线程中使用mciSendString播放音乐，你必须保证所有的mciSendString方法调用都在一个线程中进行)
+//因此暂时放弃使用多线程操作MCI
+//参考:https://blog.csdn.net/daeba/article/details/43229115?spm=1001.2014.3001.5502
+
 namespace MinConsoleNative
 {
     class AudioPool
