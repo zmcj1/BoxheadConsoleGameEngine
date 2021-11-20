29d2e15ac8472a0066a2ba13ab117f1c7880fec4
GreatDestroyerCharlie <1669247240@qq.com>
2021-11-20 12:11:44 +0300

    Update ConsoleFPSSample2.cpp

--- src/MinConsoleNativeExample/ConsoleFPSSample2.cpp
+++ src/MinConsoleNativeExample/ConsoleFPSSample2.cpp
@@ -1,4 +1,6 @@
 ﻿#include "MinConsoleNative.hpp"
+#include <cmath>
+#include <list>
 
 //参考:
 //FPS:https://www.youtube.com/watch?v=xW8skO7MFYw
@@ -146,6 +148,21 @@ public:
     }
 };
 
+struct sObject
+{
+public:
+    float x;
+    float y;
+    olcSprite* sprite;
+
+    sObject(float x, float y, olcSprite* sprite)
+    {
+        this->x = x;
+        this->y = y;
+        this->sprite = sprite;
+    }
+};
+
 class ConsoleFPS2 : public ConsoleEngine
 {
 private:
@@ -161,8 +178,8 @@ private:
     int mapHeight = 32;
 
     //player:
-    float playerX = 6.0f;           // Player Start Position
-    float playerY = 12.0f;
+    float playerX = 8.0f;           // Player Start Position
+    float playerY = 14.7f;
     float playerAngle = 0.0f;       // Player Start Rotation
     float moveSpeed = 5.0f;         // Walking Speed
     float rotateSpeed = Math::PI;   // Rotating Speed (1 sec 180 degrees)
@@ -179,6 +196,12 @@ private:
     olcSprite* spriteWall;
     olcSprite* spriteLamp;
     olcSprite* spriteFireBall;
+
+    //palette:
+    ConsolePalette legacyPalette = paletteSystem.palettes[PaletteType::Legacy];
+
+    //objects:
+    list<sObject> listObjects;
 
 public:
     void Init(int consoleWidth, int consoleHeight)
@@ -234,6 +257,13 @@ public:
         this->spriteWall = new olcSprite(L"../../res/olcSprites/fps_wall1.spr");
         this->spriteLamp = new olcSprite(L"../../res/olcSprites/fps_lamp1.spr");
         this->spriteFireBall = new olcSprite(L"../../res/olcSprites/fps_fireball1.spr");
+
+        listObjects =
+        {
+            sObject(8.5f, 8.5f, this->spriteLamp),
+            sObject(7.5f, 7.5f, this->spriteLamp),
+            sObject(10.5f, 3.5f, this->spriteLamp),
+        };
     }
 
     void ClampMouseInClient()
@@ -328,7 +358,7 @@ public:
                 ClampMouseInClient();
             }
             //rotate
-            playerAngle += diffX * rotateSpeed * deltaTime * 0.1f;
+            playerAngle += diffX * rotateSpeed * deltaTime * 0.05f;
         }
         //movement forward
         if (Input::GetKey('W'))
@@ -364,15 +394,19 @@ public:
         {
             float rayAngle = (playerAngle - FOV / 2.0f) + ((float)x / consoleWidth) * FOV;
 
+            float stepSize = 0.01f;
             float distanceToWall = 0.0f;
+
             bool hitWall = false;
 
             float eyeX = ::cosf(rayAngle);
             float eyeY = ::sinf(rayAngle);
 
+            float sampleX = 0.0f;
+
             while (!hitWall && distanceToWall < depth)
             {
-                distanceToWall += 0.1f;
+                distanceToWall += stepSize;
 
                 int testX = (int)(playerX + eyeX * distanceToWall);
                 int testY = (int)(playerY + eyeY * distanceToWall);
@@ -382,15 +416,42 @@ public:
                     hitWall = true;
                     distanceToWall = depth;
                 }
+                //与墙壁发生了碰撞
                 else if (map[testY * mapWidth + testX] == L'#')
                 {
                     hitWall = true;
+
+                    // 确定射线击中墙壁的位置:
+
+                    //墙壁中心点(假设每一堵墙壁都是1X1大小)
+                    float blockMidX = (float)testX + 0.5f;
+                    float blockMidY = (float)testY + 0.5f;
+                    //射线命中点
+                    float testPointX = playerX + eyeX * distanceToWall;
+                    float testPointY = playerY + eyeY * distanceToWall;
+
+                    //testAngle为从block中点出发, 射向射线命中点的向量与X轴的夹角
+                    float testAngle = atan2f(testPointY - blockMidY, testPointX - blockMidX);
+
+                    //命中右方
+                    if (testAngle >= -3.14159f * 0.25f && testAngle < 3.14159f * 0.25f)
+                        sampleX = testPointY - (float)testY;
+                    //命中上方
+                    if (testAngle >= 3.14159f * 0.25f && testAngle < 3.14159f * 0.75f)
+                        sampleX = testPointX - (float)testX;
+                    //命中下方
+                    if (testAngle < -3.14159f * 0.25f && testAngle >= -3.14159f * 0.75f)
+                        sampleX = testPointX - (float)testX;
+                    //命中左方
+                    if (testAngle >= 3.14159f * 0.75f || testAngle < -3.14159f * 0.75f)
+                        sampleX = testPointY - (float)testY;
                 }
             }
 
             int ceiling = (int)(consoleHeight / 2.0f - consoleHeight / distanceToWall);
             int floor = consoleHeight - ceiling;
 
+#ifdef OLD_SHADER
             for (int y = 0; y < consoleHeight; y++)
             {
                 //draw ceiling
@@ -408,7 +469,6 @@ public:
                     else if (distanceToWall <= depth / 2.0f) wallShade = medium_shade;
                     else if (distanceToWall < depth) wallShade = light_shade;
                     else wallShade = L' ';
-
                     crenderer->Draw({ x, y }, Cell(wallShade, { 255,0,0 }, { 0, 0, 0 }));
                 }
                 //draw floor
@@ -423,8 +483,106 @@ public:
                     else if (floorDistance <= 0.75f) floorShade = L'.';
                     else if (floorDistance <= 0.9f) floorShade = L'-';
                     else floorShade = L' ';
-
                     crenderer->Draw({ x, y }, Cell(floorShade, { 128, 0, 0 }, { 0, 0, 0 }));
+                }
+            }
+#else
+            for (int y = 0; y < consoleHeight; y++)
+            {
+                //draw ceiling
+                if (y <= ceiling)
+                {
+                    crenderer->Draw({ x, y }, Cell(L' ', { 0,0,0 }, { 0, 0, 255 }));
+                }
+                //draw wall
+                else if (y > ceiling && y <= floor)
+                {
+                    if (distanceToWall < depth)
+                    {
+                        float sampleY = ((float)y - (float)ceiling) / ((float)floor - (float)ceiling);
+                        ConsoleColorPair pair = MinUshortToConsoleColor(spriteWall->SampleColour(sampleX, sampleY));
+                        Color24 _foreColor24 = legacyPalette.consolePalette[pair.foreColor];
+                        Color24 _backColor24 = legacyPalette.consolePalette[pair.backColor];
+                        crenderer->Draw({ x, y }, Cell(spriteWall->SampleGlyph(sampleX, sampleY), _foreColor24, _backColor24));
+                    }
+                    else
+                    {
+                        crenderer->Draw({ x, y }, Cell(L' ', { 0,0,0 }, { 0,0,0 }));
+                    }
+                }
+                //draw floor
+                else
+                {
+                    crenderer->Draw({ x, y }, Cell(L' ', { 0, 0, 0 }, { 0, 128, 0 }));
+                }
+            }
+#endif
+        }
+
+        //draw objects:
+        for (const auto& object : listObjects)
+        {
+            // Can object be seen?
+            float vecX = object.x - playerX;
+            float vecY = object.y - playerY;
+            float distanceFromPlayer = sqrtf(vecX * vecX + vecY * vecY);
+
+            float eyeX = cosf(playerAngle);
+            float eyeY = sinf(playerAngle);
+
+            //玩家角度 - 玩家到物体的向量的角度
+            //float objectAngle = atan2f(eyeY, eyeX) - atan2f(vecY, vecX);
+            float objectAngle = atan2f(eyeX, eyeY) - atan2f(vecX, vecY);
+
+            //限制取值范围在+PI与-PI之间
+            if (objectAngle < -3.14159f)
+                objectAngle += 2.0f * 3.14159f;
+            if (objectAngle > 3.14159f)
+                objectAngle -= 2.0f * 3.14159f;
+
+            bool inPlayerFOV = fabs(objectAngle) < FOV / 2.0f;
+
+            //画在视野范围之内但是不要太近的物体, 不画超过视距的物体
+            if (inPlayerFOV && distanceFromPlayer >= 0.5f && distanceFromPlayer < depth)
+            {
+                float objectCeiling = (float)(consoleHeight / 2.0) - consoleHeight / ((float)distanceFromPlayer);
+                float objectFloor = consoleHeight - objectCeiling;
+                float objectHeight = objectFloor - objectCeiling;
+
+                float objectAspectRatio = (float)object.sprite->nHeight / (float)object.sprite->nWidth;
+                float objectWidth = objectHeight / objectAspectRatio;
+                float middleOfObject = (0.5f * (objectAngle / (FOV / 2.0f)) + 0.5f) * (float)consoleWidth;
+
+                // Draw Lamp
+                for (float lx = 0; lx < objectWidth; lx++)
+                {
+                    for (float ly = 0; ly < objectHeight; ly++)
+                    {
+                        float sampleX = lx / objectWidth;
+                        float sampleY = ly / objectHeight;
+                        wchar c = object.sprite->SampleGlyph(sampleX, sampleY);
+
+                        int nObjectColumn = (int)(middleOfObject + lx - (objectWidth / 2.0f));
+
+                        if (nObjectColumn >= 0 && nObjectColumn < consoleWidth)
+                        {
+                            //if (c != L' ' && fDepthBuffer[nObjectColumn] >= distanceFromPlayer)
+                            //{
+                            //    ConsoleColorPair pair = MinUshortToConsoleColor(spriteWall->SampleColour(sampleX, sampleY));
+                            //    Color24 _foreColor24 = legacyPalette.consolePalette[pair.foreColor];
+                            //    Color24 _backColor24 = legacyPalette.consolePalette[pair.backColor];
+                            //    crenderer->Draw({ nObjectColumn, objectCeiling + ly }, Cell(c, _foreColor24, _backColor24));
+                            //    fDepthBuffer[nObjectColumn] = fDistanceFromPlayer;
+                            //}
+
+                            {
+                                ConsoleColorPair pair = MinUshortToConsoleColor(object.sprite->SampleColour(sampleX, sampleY));
+                                Color24 _foreColor24 = legacyPalette.consolePalette[pair.foreColor];
+                                Color24 _backColor24 = legacyPalette.consolePalette[pair.backColor];
+                                crenderer->Draw({ nObjectColumn, (int)(objectCeiling + ly) }, Cell(c, _foreColor24, _backColor24));
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -459,8 +617,17 @@ public:
 int main()
 {
     ConsoleFPS2 consoleFPS;
-    COORD realSize = consoleFPS.ConstructConsole(L"ConsoleFPS2", PaletteType::Legacy, 0, 0, 8, 8, L"Consolas", FW_NORMAL, true);
+
+#ifdef FULL_SCREEN
+    COORD realSize = consoleFPS.ConstructConsole(L"ConsoleFPS2", PaletteType::Legacy, 0, 0, 4, 4, L"Consolas", FW_NORMAL, true);
     consoleFPS.Init(realSize.X, realSize.Y);
+#else
+    COORD realSize = { 360, 120 };
+    consoleFPS.ConstructConsole(L"ConsoleFPS2", PaletteType::Legacy, realSize.X, realSize.Y, 4, 4, L"Consolas", FW_NORMAL);
+    POINT s = window.GetClientSize();
+    consoleFPS.Init(realSize.X, realSize.Y);
+#endif
+
     consoleFPS.StartLoop();
     return 0;
 }
