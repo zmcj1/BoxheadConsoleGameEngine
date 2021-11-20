8b6f6b70856c2e4520196dc304a25930e5f7347a
GreatDestroyerCharlie <1669247240@qq.com>
2021-11-20 08:53:42 +0300

    Update ConsoleFPSSample2.cpp

--- src/MinConsoleNativeExample/ConsoleFPSSample2.cpp
+++ src/MinConsoleNativeExample/ConsoleFPSSample2.cpp
@@ -1,11 +1,150 @@
 ﻿#include "MinConsoleNative.hpp"
 
+//参考:
+//FPS:https://www.youtube.com/watch?v=xW8skO7MFYw
+//FPS2:https://www.youtube.com/watch?v=HEb2akswCcw
+
 using namespace std;
 
 #define light_shade     0x2591
 #define medium_shade    0x2592
 #define dark_shade      0x2593
 #define full_shade      0x2588
+
+//Inspired by olcConsoleGameEngine.
+class olcSprite
+{
+public:
+    int nWidth = 0;
+    int nHeight = 0;
+
+    olcSprite()
+    {
+
+    }
+
+    olcSprite(int w, int h)
+    {
+        Create(w, h);
+    }
+
+    olcSprite(const std::wstring& sFile)
+    {
+        if (!Load(sFile))
+            Create(8, 8);
+    }
+
+private:
+    short* m_Glyphs = nullptr;
+    short* m_Colours = nullptr;
+
+    void Create(int w, int h)
+    {
+        nWidth = w;
+        nHeight = h;
+        m_Glyphs = new short[w * h];
+        m_Colours = new short[w * h];
+        for (int i = 0; i < w * h; i++)
+        {
+            m_Glyphs[i] = L' ';
+            m_Colours[i] = FG_BLACK;
+        }
+    }
+
+public:
+    void SetGlyph(int x, int y, short c)
+    {
+        if (x < 0 || x >= nWidth || y < 0 || y >= nHeight)
+            return;
+        else
+            m_Glyphs[y * nWidth + x] = c;
+    }
+
+    void SetColour(int x, int y, short c)
+    {
+        if (x < 0 || x >= nWidth || y < 0 || y >= nHeight)
+            return;
+        else
+            m_Colours[y * nWidth + x] = c;
+    }
+
+    short GetGlyph(int x, int y)
+    {
+        if (x < 0 || x >= nWidth || y < 0 || y >= nHeight)
+            return L' ';
+        else
+            return m_Glyphs[y * nWidth + x];
+    }
+
+    short GetColour(int x, int y)
+    {
+        if (x < 0 || x >= nWidth || y < 0 || y >= nHeight)
+            return FG_BLACK;
+        else
+            return m_Colours[y * nWidth + x];
+    }
+
+    short SampleGlyph(float x, float y)
+    {
+        int sx = (int)(x * (float)nWidth);
+        int sy = (int)(y * (float)nHeight - 1.0f);
+        if (sx < 0 || sx >= nWidth || sy < 0 || sy >= nHeight)
+            return L' ';
+        else
+            return m_Glyphs[sy * nWidth + sx];
+    }
+
+    short SampleColour(float x, float y)
+    {
+        int sx = (int)(x * (float)nWidth);
+        int sy = (int)(y * (float)nHeight - 1.0f);
+        if (sx < 0 || sx >= nWidth || sy < 0 || sy >= nHeight)
+            return FG_BLACK;
+        else
+            return m_Colours[sy * nWidth + sx];
+    }
+
+    bool Save(std::wstring sFile)
+    {
+        FILE* f = nullptr;
+        _wfopen_s(&f, sFile.c_str(), L"wb");
+        if (f == nullptr)
+            return false;
+
+        fwrite(&nWidth, sizeof(int), 1, f);
+        fwrite(&nHeight, sizeof(int), 1, f);
+        fwrite(m_Colours, sizeof(short), nWidth * nHeight, f);
+        fwrite(m_Glyphs, sizeof(short), nWidth * nHeight, f);
+
+        fclose(f);
+
+        return true;
+    }
+
+    bool Load(const std::wstring& sFile)
+    {
+        delete[] m_Glyphs;
+        delete[] m_Colours;
+        nWidth = 0;
+        nHeight = 0;
+
+        FILE* f = nullptr;
+        _wfopen_s(&f, sFile.c_str(), L"rb");
+        if (f == nullptr)
+            return false;
+
+        std::fread(&nWidth, sizeof(int), 1, f);
+        std::fread(&nHeight, sizeof(int), 1, f);
+
+        Create(nWidth, nHeight);
+
+        std::fread(m_Colours, sizeof(short), nWidth * nHeight, f);
+        std::fread(m_Glyphs, sizeof(short), nWidth * nHeight, f);
+
+        std::fclose(f);
+        return true;
+    }
+};
 
 class ConsoleFPS2 : public ConsoleEngine
 {
@@ -32,6 +171,7 @@ private:
     const float FOV = Math::PI / 4; // Field of view
     const float depth = 16.0f;      // Maximum rendering distance
 
+    //mouse support:
     bool mouseSupport = true;
     bool mouseLockMode = true;
 
